056e730f72def14d3465e880d212a77cb175315f
Thriveni45 <purellathriveni2000@gmail.com>
2025-07-02 14:56:06 +0530

    Add files via upload

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,23 @@
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        // Primitive data types
+        byte myByte = 100;
+        short myShort = 10000;
+        int myInt = 123456;
+        long myLong = 123456789L;
+        float myFloat = 12.34f;
+        double myDouble = 1234.5678;
+        char myChar = 'A';
+        boolean myBoolean = true;
+
+        // Printing the values
+        System.out.println("Byte value: " + myByte);
+        System.out.println("Short value: " + myShort);
+        System.out.println("Int value: " + myInt);
+        System.out.println("Long value: " + myLong);
+        System.out.println("Float value: " + myFloat);
+        System.out.println("Double value: " + myDouble);
+        System.out.println("Char value: " + myChar);
+        System.out.println("Boolean value: " + myBoolean);
+    }
+}
